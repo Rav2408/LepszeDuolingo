24176c86b156316a9ace0320d08f0e8f0c9b96ae
miloszAlejster <milosz.alejster@gmail.com>
2022-12-01 05:06:02 +0100

    added login and register activity

--- app/src/main/java/pl/edu/pb/lepszeduolingo/LoginActivity.java
+++ app/src/main/java/pl/edu/pb/lepszeduolingo/LoginActivity.java
@@ -2,13 +2,30 @@ package pl.edu.pb.lepszeduolingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
-
+    Button getBack;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getBack = (Button) findViewById(R.id.getBackLoginButton);
+        getBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, TitleActivity.class));
+            }
+        });
+        loginButton = (Button) findViewById(R.id.confirmLoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // TODO: put login logic here
+            }
+        });
     }
 }
