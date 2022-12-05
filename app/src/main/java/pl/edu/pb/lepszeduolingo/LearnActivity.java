3ce9270c40eeb9ec22345bf5673ecc2d9a270ce2
miloszAlejster <milosz.alejster@gmail.com>
2022-12-05 16:20:23 +0100

    added check for answer

--- app/src/main/java/pl/edu/pb/lepszeduolingo/LearnActivity.java
+++ app/src/main/java/pl/edu/pb/lepszeduolingo/LearnActivity.java
@@ -1,16 +1,25 @@
 package pl.edu.pb.lepszeduolingo;
-
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Objects;
 
 public class LearnActivity extends AppCompatActivity {
     Button answerView_1, answerView_2, answerView_3, answerView_4;
     TextView questionViewText;
     CardView questionWrapper;
+    MutableLiveData<Integer> answer = new MutableLiveData<>();
+    // test data
+    Integer correctAnswer = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,20 @@ public class LearnActivity extends AppCompatActivity {
         questionViewText = findViewById(R.id.question_view);
         questionWrapper = findViewById(R.id.question_wrapper);
         setParams();
+        // default
+        answer.setValue(-1);
+        getAnswer();
+        // listen for answer
+        answer.observe(this, new Observer<Integer>(){
+            @Override
+            public void onChanged(Integer chosenAnswer) {
+                if(Objects.equals(chosenAnswer, correctAnswer)){
+                    Log.d(TAG, String.format("Good answer: %2d", chosenAnswer));
+                }else{
+                    Log.d(TAG, String.format("Wrong Answer: %2d", chosenAnswer));
+                }
+            }
+        });
     }
     void setParams(){
         // TODO: pass correct params
@@ -37,5 +60,31 @@ public class LearnActivity extends AppCompatActivity {
         answerView_2.setText("nie");
         answerView_3.setText("tej");
         answerView_4.setText("wuchta wiary");
+    }
+    void getAnswer(){
+        answerView_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setValue(0);
+            }
+        });
+        answerView_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setValue(1);
+            }
+        });
+        answerView_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setValue(2);
+            }
+        });
+        answerView_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                answer.setValue(3);
+            }
+        });
     }
 }
