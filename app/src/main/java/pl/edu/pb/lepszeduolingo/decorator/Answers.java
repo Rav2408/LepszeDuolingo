f2c49d99930dccbd6b44d3fa3308bc806ebb3954
rciup <rciupek0@gmail.com>
2023-01-18 15:29:40 +0100

    decorator update

--- app/src/main/java/pl/edu/pb/lepszeduolingo/decorator/Answers.java
+++ app/src/main/java/pl/edu/pb/lepszeduolingo/decorator/Answers.java
@@ -4,4 +4,5 @@ import pl.edu.pb.lepszeduolingo.models.*;
 public interface Answers {
     public String getAnswer(int i);
     public String getCorrectAnswer();
+    public void setAnswer(int position, String answer);
 }
