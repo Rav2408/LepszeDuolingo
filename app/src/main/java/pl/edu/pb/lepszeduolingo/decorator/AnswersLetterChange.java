80ebbd28067ea777777527c983118c64d56c2f93
krzbb <kerazeb@gmail.com>
2023-01-19 12:55:22 +0100

    decorator adjustment

--- app/src/main/java/pl/edu/pb/lepszeduolingo/decorator/AnswersLetterChange.java
+++ app/src/main/java/pl/edu/pb/lepszeduolingo/decorator/AnswersLetterChange.java
@@ -23,35 +23,30 @@ public class AnswersLetterChange extends AnswersDecorator {
             if (!answer.equals(getCorrectAnswer())) {
                 if (answer.contains("t") && !answer.contains("tt")) {
                     if (new Random().nextDouble() < 0.05) {
-                        Log.d("letterchange", "dupa");
                         answerList.set(i, answer.replace("t", "tt"));
                     }
                 }
                 if (answer.contains("ch")) {
                     if (new Random().nextDouble() < 0.05) {
                         answer.replace("ch", "h");
-                        Log.d("letterchange", "dupa");
                         answerList.set(i, answer.replace("ch", "h"));
                     }
                 }
                 if (answer.contains("c")) {
                     if (new Random().nextDouble() < 0.05) {
                         answer.replace("c", "k");
-                        Log.d("letterchange", "dupa");
                         answerList.set(i, answer.replace("c", "k"));
                     }
                 }
                 if (answer.contains("u")) {
                     if (new Random().nextDouble() < 0.05) {
                         answer.replace("u", "ou");
-                        Log.d("letterchange", "dupa");
                         answerList.set(i, answer.replace("u", "ou"));
                     }
                 }
                 if (answer.contains("h") && !answer.contains("ch")) {
                     if (new Random().nextDouble() < 0.05) {
                         answer.replace("h", "ch");
-                        Log.d("letterchange", "dupa");
                         answerList.set(i, answer.replace("h", "ch"));
                     }
                 }
